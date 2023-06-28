3bbd40c11df1e3ebdbc61d7705ddef3f56b12bb9
Saimongu Marma <124234753+Saimongu007@users.noreply.github.com>
2023-06-28 23:16:19 +0600

    area and circumference of a circle.

---
+++ vjg 5th.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double r;
+    cin>>r;
+    double A = M_PI*r*r;
+    double C = 2*M_PI*r;
+    cout<<fixed<<setprecision(6)<<A<<" "<<fixed<<setprecision(6)<<C<<endl;
+    return 0;
+}
