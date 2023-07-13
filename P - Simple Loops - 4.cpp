daa5b88a032d13356895e084a997dcc07f0845ca
Saimongu Marma <124234753+Saimongu007@users.noreply.github.com>
2023-07-14 04:18:56 +0600

    ascending order zero zero break

---
+++ P - Simple Loops - 4.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x,y;
+
+    while(cin>> x >> y)
+    {
+        if(x == 0 && y == 0)
+        {
+            break;
+        }
+        cout<< min(x,y) << " "<< max(x,y) <<endl;
+    }
+    return 0;
+}
