09d08abb7c6686ba04a7c64a3576db2d14a6e267
Saimongu Marma <124234753+Saimongu007@users.noreply.github.com>
2023-07-30 23:20:05 +0600

    height of H cm and a width of W cm. single '#'.

---
+++ AP - Nested Loops - 1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int H, W;
+
+    while (cin>>H>>W)
+    {
+        if (H == 0 && W == 0)
+            break;
+
+        for (int i = 0; i < H; i++)
+        {
+            for (int j = 0; j < W; j++)
+            {
+                cout << "#";
+            }
+            cout << endl;
+        }
+        if (H == 0 && W == 0)
+            break;
+
+        cout << endl;
+    }
+
+    return 0;
+}
