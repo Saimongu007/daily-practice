a4a84578d2fbbef63c3ccf3672ffcfdc88266498
Saimongu Marma <124234753+Saimongu007@users.noreply.github.com>
2023-11-21 00:38:41 +0600

    Find submission.

---
+++ A-A-vjudge-contest.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>> t;
+
+    while(t--)
+    {
+        int x;
+        cin>> x;
+
+        int s = x * 60;
+
+        int sub = s /30;
+
+        cout<< sub <<endl;
+    }
+}
